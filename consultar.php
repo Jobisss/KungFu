d7f62b76de42dcf5090d664285237947076ca9d4
Jobisss <jgpschianti07@gmail.com>
2022-11-11 08:39:09 -0300

    consultar

---
+++ consultar.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>Consultar</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Consultar</h1>
+        <h3>Alunos cadastrados</h3>
+
+        <table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">Nome</th>
+      <th scope="col">Foto</th>
+      <th scope="col">Ano</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+        </tbody>
+        </table>
+    </div>
+</body>
+</html>
